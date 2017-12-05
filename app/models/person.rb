e54b62e6544837ad6823209356ce7f5a9a428322
everysum1 <israeldmatos@gmail.com>
2017-12-05 02:11:21 -0800

    Add model w/ methods to calculate age & projected benefits

---
+++ app/models/person.rb
@@ -0,0 +1,32 @@
+class Person < ApplicationRecord
+  before_validation :set_age
+  before_save :calculate_first_SS_check
+
+  def :set_age
+    self[:age] = Date.today - self[:birthday]
+  end
+
+  def :calculate_first_SS_check
+    # figure out income base
+    backwards_project_income # populate earnings history
+    forwards_project_income
+    # calculate Average Monthly Indexed Earnings (top 35 earning years)
+    average_monthly_indexed_earnings = calculate_AMIE(self[:age], self[:current_income])
+    # convert AMIE to benefits
+    monthly_income = convert_AMIE_to_benefits(AMIE)
+    # calculate spouse
+    # adjust benefits according to year they take
+    # benefit reduction +/- 36 months
+
+    self[:estimated_SS_benefit] = monthly_income
+  end
+
+  def calculate_AMIE(age, current_income)
+
+  end
+
+  def convert_AMIE_to_benefits(monthly_earnings)
+
+  end
+
+end
