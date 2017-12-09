1e83dae1c4e2119abebc29e992e60f0fc8112eaf
everysum1 <israeldmatos@gmail.com>
2017-12-09 15:29:50 -0800

    Fix set_age method, add projected income methods outline

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,23 +1,27 @@
 class Person < ApplicationRecord
-  before_validation :set_age, :set_life_expectancy
-  before_save :calculate_first_SS_check
+  has_many :income_years
+  accepts_nested_attributes_for :income_year
+  # before_save :calculate_first_SS_check
 
-  def :set_age
-    self[:age] = Date.today - self[:birthday]
+  def set_age(birthday)
+    self[:age] = ((Date.today - birthday.to_date)/365.25).to_i
   end
 
-  def :set_life_expectancy
-    if self[:sex] == 'M'
+  def set_life_expectancy(sex)
+    if sex == 'M'
       self[:life_expectancy] = 76
     else
       self[:life_expectancy] = 81
     end
   end
 
-  def :calculate_first_SS_check
+  def calculate_first_SS_check
     # figure out income base
-    backwards_project_income # populate earnings history
-    forwards_project_income
+    current_age = self[:age]
+    years_backwards = current_age - 22
+    years_forwards = 67 - current_age
+    backwards_project_income(years_backwards) # populate earnings history
+    forwards_project_income(years_forwards)
     # calculate Average Monthly Indexed Earnings (top 35 earning years)
     average_monthly_indexed_earnings = calculate_AMIE(self[:age], self[:current_income])
     # convert AMIE to benefits
@@ -30,6 +34,21 @@ class Person < ApplicationRecord
   end
 
   private
+
+    def backwards_project_income(years)
+      # for every year between current age and 22
+        # create an IncomeYear instance
+        # with previous year's income minus 2%
+        # save it in current person's income_year array
+    end 
+
+    def forwards_project_income(years)
+      # for every year between current age and 67
+        # create an IncomeYear instance
+        # with previous year's income plus 2%
+        # save it in current person's income_year array
+    end
+
     def calculate_AMIE(age, current_income)
 
     end
