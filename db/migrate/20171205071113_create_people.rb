4d4b36e18c9ae78db5ab91bbc5ed62f46c8eec78
everysum1 <israeldmatos@gmail.com>
2017-12-09 13:19:38 -0800

    Add sex field to person migration

--- db/migrate/20171205071113_create_people.rb
+++ db/migrate/20171205071113_create_people.rb
@@ -2,12 +2,13 @@ class CreatePeople < ActiveRecord::Migration[5.0]
   def change
     create_table :people do |t|
       t.integer :age
+      t.string :sex
       t.date :birthday
       t.float :current_income
       t.integer :life_expectancy
       t.date :claim_date
       t.boolean :spousal_benefits
-      t.float :estimated_SS_benefit
+      t.float :estimated_ss_benefit
 
       t.timestamps
     end
