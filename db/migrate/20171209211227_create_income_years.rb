f0f207a9ca1007eab257aa6728c6039c0c207e46
everysum1 <israeldmatos@gmail.com>
2017-12-09 13:22:58 -0800

    Add reference to person model in income year migration

--- db/migrate/20171209211227_create_income_years.rb
+++ db/migrate/20171209211227_create_income_years.rb
@@ -3,6 +3,7 @@ class CreateIncomeYears < ActiveRecord::Migration[5.0]
     create_table :income_years do |t|
       t.string :year
       t.float :income
+      t.references :person, foreign_key: true
 
       t.timestamps
     end
